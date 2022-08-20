df4b777ee4f277ceee03c64974fd0838e7c85a85
daniel_eln <omoniyiopemipo6@gmail.com>
2022-08-20 22:09:38 +0100

    Implimented the highlight on keyup function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const suggestions = document.querySelector('.suggestions');
 const search = document.querySelector('.search');
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 let cities = [];
+let wordToMatch;
 
 fetch(endpoint)
     .then(blob => blob.json())
@@ -9,7 +10,7 @@ fetch(endpoint)
     .catch(err => console.log(err));
 
 function findMatches(input) {
-    const wordToMatch = new RegExp(input, 'gi');
+    wordToMatch = new RegExp(input, 'gi');
     return cities.filter(place => {
        const isMatch = `${place.city} ${place.state}`.match(wordToMatch) !== null; 
        if (isMatch )  return place;
@@ -21,8 +22,13 @@ function displayMatches(){
     console.log(matches);
 
     suggestions.innerHTML = matches.map(match => {
+        // Replace wordToMatch, with a span with class hl 
+        const state = match.state.replace(wordToMatch, `<span class = 'hl'>${this.value}</span>`);
+        const city = match.city.replace(wordToMatch, `<span class = 'hl'>${this.value}</span>`);
+
+
         return `<li>
-        <span class='name'>${match.city}, ${match.state}</span>
+        <span class='name'>${city}, ${state}</span>
         <span class='population'>${match.population}</span>
       </li>`;
     }).join('');
